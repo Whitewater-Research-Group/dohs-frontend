3b9c7b0500bfeff8d82a6d05ee2d68cc6801f5f6
obuhdaniel <danielobuh05@gmail.com>
2024-08-11 23:04:49 +0100

    first commit with docker

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api', dashboardRoutes);  
-
+app.get('/', function(req, res){
+    res.send('Hello, Developer. your docker image is working!')
+})
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
