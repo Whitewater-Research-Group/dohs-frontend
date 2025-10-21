5fa62f9d7b105676f91d56f4d47f4414cddaf86a
xavierscript <david111francisco@gmail.com>
2025-10-21 12:33:22 +0100

    removed status column

--- src/pages/(stakeholders)/dashboard/HumanCases.jsx
+++ src/pages/(stakeholders)/dashboard/HumanCases.jsx
@@ -459,9 +459,6 @@ function Cases() {
                           Classification
                         </th>
                         <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                          Status
-                        </th>
-                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                           Outcome
                         </th>
                         <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
